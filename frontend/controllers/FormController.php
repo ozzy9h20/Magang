312369d3e57b34a1bb095535d1d9395fe4e2bbf3
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-11-01 13:18:34 +0700

    Working with Form
    Test URL:
    http://localhost:8081/yii-application/frontend/web/index.php?r=form/entry

---
+++ frontend/controllers/FormController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use frontend\models\EntryForm;
+
+class FormController extends Controller
+{
+    // ...existing code...
+
+    public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // valid data received in $model
+
+            // do something meaningful here about $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // either the page is initially displayed or there is some validation error
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+}
